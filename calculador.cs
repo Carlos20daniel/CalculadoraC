6b1a902c03fafd72b8d167259722aec092bf7659
Carlos20daniel <1440648@senati.pe>
2023-09-08 08:57:10 -0500

    insetamos teclado

--- calculador.cs
+++ calculador.cs
@@ -7,10 +7,53 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
-public class Class1
+namespace CALCULADORA
 {
-	public Class1()
-	{
-	}
-}
+    public partial class Form1 : Form
+    {
+        double primero = 0, segundo = 0;
+        string operador = "";
+        public Calculadora()
+        {
+            private void button0_Click(object sender, EventArgs e)
+            {
+                Resultado.Text += "0";
+            }
+            private void button1_Click(object sender, EventArgs e)
+            {
+                Resultado.Text += "1";
+            }
+            private void button2_Click(object sender, EventArgs e)
+            {
+                Resultado.Text += "2";
+            }
+            private void button3_Click(object sender, EventArgs e)
+            {
+                Resultado.Text += "3";
+            }
+            private void button4_Click(object sender, EventArgs e)
+            {
+                Resultado.Text += "4";
+            }
+            private void button5_Click(object sender, EventArgs e)
+            {
+                Resultado.Text += "5";
+            }
+            private void button6_Click(object sender, EventArgs e)
+                Resultado.Text += "6";
+            }
+            private void button7_Click(object sender, EventArgs e)
+            {
+            Resultado.Text += "7";
+            }
+            private void button8_Click(object sender, EventArgs e)
+            {
+            Resultado.Text += "8";
+            }
+            private void button8_Click(object sender, EventArgs e)
+            {
+            Resultado.Text += "9";
+            }
+        }
+    }
+} 
